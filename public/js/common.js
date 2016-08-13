f037156aec89453a67ad1c58ad0b22f3915bc0eb
Ricky <ricky840@gmail.com>
2016-08-13 09:08:27 +0000

    media report complete

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,12 @@
+function getFormattedDate(date) {
+  var year = date.getFullYear();
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : '0' + day;
+  return month + '/' + day + '/' + year;
+}
+
 //for notification hide rather than dismiss
 $(function() {
 	$("[data-hide]").on("click", function(){
